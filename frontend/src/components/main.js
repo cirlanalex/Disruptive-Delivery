b32d9157e7f4f5592154184306d3135669fb67b0
Alexandru Cirlan <propvpgamernl@gmail.com>
2023-01-18 13:32:41 +0100

    updates to db

--- frontend/src/components/main.js
+++ frontend/src/components/main.js
@@ -7,9 +7,7 @@ const Main = () => {
         <br/>
         <br/>
         <br/>
-        <h1>Our vision is to modernize traditional delivery, track your package from the hands of the sender to
-your doorstep and make use of our reliable, carbon-neutral, worldwide, technology-focused delivery
-service.</h1>
+        <h1>A reliable and transparent delivery service.</h1>
     </center>
     )
 };
